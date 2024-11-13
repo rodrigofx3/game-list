303bfc5e54f3f1d9933f0a5fce4b8e627dcd73a6
Rodrigo Faria Ramos <rodframos@outlook.com>
2024-11-12 21:51:50 -0300

    Commit inicial

---
+++ src/test/java/dev/rodrigoramos/game_list/GameListApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.rodrigoramos.game_list;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GameListApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
